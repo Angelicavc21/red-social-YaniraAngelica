a8666667b654653fa70bbb64fb86a58427f6ab3b
YaniraAB <arenazas2.2@hotmail.com>
2018-01-09 12:54:14 -0500

    Añadiendo maquetado y funcinalidad a profile.js/html/css

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,7 @@
 $(document).ready(begin());
 
 function begin() {
+  //  Materialize
   $('.button-collapse').sideNav();
   $('.dropdown-button').dropdown({
     inDuration: 300,
@@ -11,5 +12,45 @@ function begin() {
     belowOrigin: false, // Displays dropdown below the button
     alignment: 'left', // Displays dropdown with edge aligned to the left of button
     stopPropagation: false // Stops event propagation
+  });
+  $('#textarea1').val('New Text');
+  $('#textarea1').trigger('autoresize');
+
+  //  Capturando etiquetas con jquery
+  var $textarea = $('#icon_prefix2');
+  var $containerPost = $('#container-post');
+  var $btnPost = $('#btn-post');
+  $btnPost.on('click', function() {
+    var $textPost = $textarea.val();
+    if ($textPost.length > 0) {
+      $containerPost.append(
+        '<div class="border-post row">' +
+          '<form class="col l10 offset-l1 offset-s1 s10">' +
+            '<div class="row">' +
+              '<div class="input-field col l11 s11">' +
+                '<i id="edit-btn" class="material-icons prefix">mode_edit</i>' +
+                '<textarea id="text-edit" class="materialize-textarea" disabled>' + $textPost + '</textarea>' +
+                '<div id="container-btn-save" class="row">' +
+                  '<span class="col l2 left"><i class="material-icons">thumb_up</i></span>' +
+                  '<span class="col l2 left"><i class="material-icons">favorite</i></span>' +
+                  '<span class="col l2 left"><i class="material-icons">fitness_center</i></span>' +
+                '</div>' +
+              '</div>' +
+            '</div>' +
+          '</form>' +
+        '</div>'
+      );
+      var $editBtn = $('#edit-btn');
+      $editBtn.on('click', function() {
+        $('#text-edit').removeAttr('disabled');
+        $('#container-btn-save').append(
+          '<a id="btn-save" class="waves-effect waves-light btn">guardar</a>'
+        );
+        $('#btn-save').on('click', function() {
+          $('#text-edit').attr('disabled', true);
+          $('#btn-save').remove();
+        });
+      });
+    }
   });
 }
