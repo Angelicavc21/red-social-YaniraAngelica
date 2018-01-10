8c885112c75d1dd3c7b910fcb4a55f08255736c9
YaniraAB <arenazas2.2@hotmail.com>
2018-01-10 10:21:25 -0500

    añadiendo detalles profile, news, html, css

--- js/news.js
+++ js/news.js
@@ -1,5 +1,6 @@
 $(document).ready(begin());
 function begin() {
+  //  Materialize
   $('#textarea1').val('New Text');
   $('#textarea1').trigger('autoresize');
   //  Habilitando post
@@ -18,16 +19,52 @@ function begin() {
                 '<i id="edit-btn" class="color-purple material-icons prefix">mode_edit</i>' +
                 '<textarea id="text-edit" class="materialize-textarea" disabled>' + $textPost + '</textarea>' +
                 '<div id="container-btn-save" class="row">' +
-                  '<span class="col l2 left"><i class="likes material-icons">thumb_up</i></span>' +
-                  '<span class="col l2 left"><i class="likes material-icons">favorite</i></span>' +
-                  '<span class="col l2 left"><i class="likes material-icons">fitness_center</i></span>' +
+                  '<span id="like" class="col l2 left"><i class="likes material-icons">thumb_up</i></span>' +
+                  '<span id="love" class="col l2 left"><i class="likes material-icons">favorite</i></span>' +
+                  '<span id="force" class="col l2 left"><i class="likes material-icons">fitness_center</i></span>' +
                   '<span class="valign-wrapper col l2 right">' + newDate.getHours() + ':' + newDate.getMinutes() + '  ' + newDate.getFullYear() + '/' + (newDate.getMonth() + 1) + '/' + newDate.getDate() + '</span>' +
+                '</div>' +
+                '<div id="container-likes" class="text-purple col s2 l2">' +
+                '</div>' +
+                '<div id="container-love" class="text-purple col s2 l2">' +
+                '</div>' +
+                '<div id="container-force" class="text-purple col s2 l2">' +
                 '</div>' +
               '</div>' +
             '</div>' +
           '</form>' +
         '</div>'
       );
+      //  habilitando btn like
+      var $like = $('#like');
+      var numLike = 1;
+      $like.on('click', function() {
+        if (numLike === 1) {
+          $('#container-likes').html('<b>' + numLike++ + ' like' + '</b>');
+        } else {
+          $('#container-likes').html('<b>' + numLike++ + ' likes' + '</b>');
+        }
+      });
+      //  habilitando btn love
+      var $love = $('#love');
+      var numLove = 1;
+      $love.on('click', function() {
+        if (numLove === 1) {
+          $('#container-love').html('<b>' + numLove++ + ' love' + '</b>');
+        } else {
+          $('#container-love').html('<b>' + numLove++ + ' loves' + '</b>');
+        }
+      });
+      //  habilitando btn fuerza
+      var $force = $('#force');
+      var numForce = 1;
+      $force.on('click', function() {
+        if (numForce === 1) {
+          $('#container-force').html('<b>' + numForce++ + ' force' + '</b>');
+        } else {
+          $('#container-force').html('<b>' + numForce++ + ' forces' + '</b>');
+        }
+      });
       var $editBtn = $('#edit-btn');
       $editBtn.on('click', function() {
         $('#text-edit').removeAttr('disabled');
